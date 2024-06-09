bf6d0eb497250f5bee7d9943c7e4a545173e262c
KoushikMadugula <A00288822@mycambrian.ca>
2024-06-09 12:31:14 -0400

    Adding Pin Zoom level Cambrian coordinates

--- Campus-Indoor-Navigation-System/MapPage.xaml.cs
+++ Campus-Indoor-Navigation-System/MapPage.xaml.cs
@@ -1,4 +1,7 @@
 namespace Campus_Indoor_Navigation_System;
+
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
 using Map = Microsoft.Maui.Controls.Maps.Map;
 
 public partial class MapPage : ContentPage
@@ -6,10 +9,22 @@ public partial class MapPage : ContentPage
 	public MapPage()
 	{
 		InitializeComponent();
-		Map map = new Map
-		{
-			MapType = Microsoft.Maui.Maps.MapType.Street,
-            IsShowingUser = true
+		
+
+        //setting cambrian college address
+        
+        var initialLocation = new Location(46.5291410, -80.9407060); // cambrian coordinates
+        MapSpan mapSpan = MapSpan.FromCenterAndRadius(initialLocation, Distance.FromMeters(10));
+        map.MoveToRegion(mapSpan);
+
+        Pin pin = new Pin
+        {
+            Label = "Cambrian College",
+            Type = PinType.Place,
+            Location = new Location(46.5291410, -80.9407060)
         };
-	}
+        map.Pins.Add(pin);
+
+
+    }
 }
